8c9a1ec498aa9c2e1e3b840327c5fc5d5581938f
Jenya Golovko <u9on1210@gmail.com>
2024-06-12 23:37:35 +0300

    My merried wedding

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function initMap() {
+  let mapReastaurant;
+  let mapMerried;
+
+  mapReastaurant = new google.maps.Map(document.getElementById('map-restaurant'),
+    {
+      center: {
+        lat: 47.86636644473956,
+        lng: 35.05962885142143,
+      },
+      zoom: 17,
+    });
+
+  mapMerried = new google.maps.Map(document.getElementById('map-merried'), {
+    center: {
+      lat: 47.83079492829225,
+      lng: 35.15930220709443,
+    },
+    zoom: 17,
+  });
+}
